23d123e325739f01801f3fce8e74dfbe77e9419f
georgh <ostankino.tower@gmail.com>
2021-06-08 20:27:41 +0800

    Initialize the database in Main

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,20 @@
 {-# OPTIONS_GHC -Wno-unused-do-bind #-}
 module Main (main) where
 
-import Options (getOptions)
+import Options (getOptions, Options (optDbFile))
 import Receiver (receive)
 import ProcessMessages (tmpFilesProcessor)
+import Database ( initializeDb )
   
-import RIO
+import RIO ( ($), Bool(True), IO, newTMVarIO )
 import Control.Concurrent (forkIO)
 
 main :: IO ()
 main = do
   opts <- getOptions
 
+  initializeDb $ optDbFile opts
+  
   -- Synchronizing mutex initialized with a value (any value) to ensure that
   -- pending messages are processed
   pendingFiles <- newTMVarIO True
